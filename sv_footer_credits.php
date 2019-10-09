35c13634eea346638e58fd1924062b827ed09573
Matthias Bathke <matthias-bathke@straightvisions.com>
2019-10-09 17:03:00 +0200

    FIX

--- sv_footer_credits.php
+++ sv_footer_credits.php
@@ -29,25 +29,25 @@
 		}
 		
 		protected function load_settings(): sv_footer_credits {
-				$this->get_setting()
-					 ->set_ID( 'disable' )
+				$this->get_setting('disable')
 					 ->set_title( __( 'Disable credits footer at all.', 'sv100_companion' ) )
 					 ->set_default_value( 0 )
 					 ->load_type( 'checkbox' );
 				
-				$this->get_setting()
-					 ->set_ID( 'text' )
+				$this->get_setting('text')
 					 ->set_title( __( 'Set your own text for credits.', 'sv100_companion' ) )
 					 ->load_type( 'text' );
 			
 			return $this;
 		}
-		public function run(){
+		public function run(): sv_footer_credits {
 			if($this->get_setting('disable')->run_type()->get_data()){
 				add_filter('sv100_sv_footer_credits', '__return_false');
 			}
 			if($this->get_setting('text')->run_type()->get_data() && strlen(trim($this->get_setting('text')->run_type()->get_data())) > 0){
 				add_filter('sv100_sv_footer_credits_text', function(){ return $this->get_setting('text')->run_type()->get_data(); });
 			}
+			
+			return $this;
 		}
 	}
